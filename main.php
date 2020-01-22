a6a445a1b059deadb3017441e11c3c1b015d4b21
ChrTor <38206602+ChrTor@users.noreply.github.com>
2020-01-22 11:51:21 +0100

    Add files via upload

--- main.php
+++ main.php
@@ -60,22 +60,6 @@
           break;
       }
     }
-
-
-// Story of John Wick
-    include "class_Human.inc";
-
-            echo "<hr>";
-
-        $Wick = new Human('John', 'Wick', 'Fist', 'Punch');
-        $Wicktim = new Human('Burt', 'Wicktim', 'Fist', 'Punch');
-
-
-
-
-
-        echo "<p>$data.Name is $Age, $Name enjoys he's $drink $color and with a lot of $info[2].</p>";
-        echo "<p> This is me mucking about with php </p>";
      ?>
 
 
